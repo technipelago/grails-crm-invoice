45fc3b9d850c77d6fcee76f009759cd7705b1143
Goran Ehrsson <goran@technipelago.se>
2015-11-18 00:45:58 +0100

    Added saveInvoice() method

--- grails-app/services/grails/plugins/crm/invoice/CrmInvoiceService.groovy
+++ grails-app/services/grails/plugins/crm/invoice/CrmInvoiceService.groovy
@@ -27,12 +27,16 @@ import org.grails.databinding.SimpleMapDataBindingSource
  */
 class CrmInvoiceService {
 
+    private static final List INVOICE_ADDRESS_BIND_WHITELIST = ['addressee'] + CrmEmbeddedAddress.BIND_WHITELIST
+
     def grailsApplication
     def grailsWebDataBinder
+    def messageSource
 
     def sequenceGeneratorService
 
     def crmCoreService
+    def crmSecurityService
     def crmTagService
 
     @Listener(namespace = "crmInvoice", topic = "enableFeature")
@@ -262,9 +266,9 @@ class CrmInvoiceService {
             grailsWebDataBinder.bind(m, customerParams as SimpleMapDataBindingSource)
         }
 
-        for(item in params.items) {
+        for (item in params.items) {
             def row = addInvoiceItem(m, item, false)
-            if(row.hasErrors()) {
+            if (row.hasErrors()) {
                 log.error("Validation error for invoice item", row.errors.allErrors.toString())
             }
         }
@@ -355,6 +359,198 @@ class CrmInvoiceService {
         return m
     }
 
+    CrmInvoice getInvoice(Long id, Long tenant = null) {
+        if (tenant == null) {
+            tenant = TenantUtils.tenant
+        }
+        final CrmInvoice m = CrmInvoice.get(id)
+        return (tenant == m?.tenantId) ? m : null
+    }
+
+    CrmInvoice findByNumber(String number, Long tenant = null) {
+        if (tenant == null) {
+            tenant = TenantUtils.tenant
+        }
+        CrmInvoice.findByNumberAndTenantId(number, tenant)
+    }
+
+    private CrmInvoice useInvoiceInstance(CrmInvoice crmInvoice = null) {
+        def tenant = TenantUtils.tenant
+        if (crmInvoice == null) {
+            crmInvoice = new CrmInvoice()
+        }
+        if (crmInvoice.tenantId) {
+            if (crmInvoice.tenantId != tenant) {
+                throw new IllegalStateException("The current tenant is [$tenant] and the specified domain instance belongs to another tenant [${crmInvoice.tenantId}]")
+            }
+        } else {
+            crmInvoice.tenantId = tenant
+        }
+        crmInvoice
+    }
+
+
+    CrmInvoice saveInvoice(CrmInvoice crmInvoice, Map params) {
+        crmInvoice = useInvoiceInstance(crmInvoice)
+        def tenant = TenantUtils.tenant
+        def currentUser = crmSecurityService.getUserInfo()
+        def oldStatus = crmInvoice.invoiceStatus
+
+        try {
+            bindDate(crmInvoice, 'invoiceDate', params.remove('invoiceDate'), currentUser?.timezone)
+            bindDate(crmInvoice, 'dueDate', params.remove('dueDate'), currentUser?.timezone)
+        } catch (CrmValidationException e) {
+            throw new CrmValidationException(e.message, crmInvoice)
+        }
+
+        // Bind "normal" properties.
+        grailsWebDataBinder.bind(crmInvoice, params as SimpleMapDataBindingSource, null, CrmInvoice.BIND_WHITELIST, null, null)
+
+        // Bind invoice address
+        if (!crmInvoice.invoice) {
+            crmInvoice.invoice = new CrmEmbeddedAddress()
+        }
+        if (params.invoice instanceof Map) {
+            grailsWebDataBinder.bind(crmInvoice.invoice, params.invoice as SimpleMapDataBindingSource, null, INVOICE_ADDRESS_BIND_WHITELIST, null, null)
+        } else {
+            grailsWebDataBinder.bind(crmInvoice.invoice, params as SimpleMapDataBindingSource, 'invoice', INVOICE_ADDRESS_BIND_WHITELIST, null, null)
+        }
+
+        // Bind delivery address.
+        if (!crmInvoice.delivery) {
+            crmInvoice.delivery = new CrmEmbeddedAddress()
+        }
+        if (params.delivery instanceof Map) {
+            grailsWebDataBinder.bind(crmInvoice.delivery, params.delivery as SimpleMapDataBindingSource, null, INVOICE_ADDRESS_BIND_WHITELIST, null, null)
+        } else {
+            grailsWebDataBinder.bind(crmInvoice.delivery, params as SimpleMapDataBindingSource, 'delivery', INVOICE_ADDRESS_BIND_WHITELIST, null, null)
+        }
+
+        if (!crmInvoice.invoice.addressee) {
+            crmInvoice.invoice.addressee = crmInvoice.customerName
+        }
+
+        // If delivery address is empty, copy invoice address (if configured to do so).
+        if (crmInvoice.delivery.empty && grailsApplication.config.crm.invoice.delivery.address.copy == 'invoice') {
+            crmInvoice.invoice.copyTo(crmInvoice.delivery)
+            if (!crmInvoice.delivery.addressee) {
+                crmInvoice.delivery.addressee = crmInvoice.invoice.addressee ?: crmInvoice.customerName
+            }
+        }
+
+        // Bind items.
+        if (params.items instanceof List) {
+            for (row in params.items) {
+                def item = row.id ? CrmInvoiceItem.get(row.id) : new CrmInvoiceItem(order: crmInvoice)
+                grailsWebDataBinder.bind(item, row as SimpleMapDataBindingSource, null, CrmInvoiceItem.BIND_WHITELIST, null, null)
+                if (item.id) {
+                    item.save()
+                } else if (!item.hasErrors()) {
+                    crmInvoice.addToItems(item)
+                }
+            }
+        } else {
+            bindItems(crmInvoice, params)
+        }
+
+        if (!crmInvoice.invoiceStatus) {
+            crmInvoice.invoiceStatus = CrmInvoiceStatus.withNewSession {
+                CrmInvoiceStatus.createCriteria().get() {
+                    eq('tenantId', tenant)
+                    order 'orderIndex', 'asc'
+                    maxResults 1
+                }
+            }
+        }
+        if (!crmInvoice.paymentTerm) {
+            crmInvoice.paymentTerm = CrmPaymentTerm.withNewSession {
+                CrmPaymentTerm.createCriteria().get() {
+                    eq('tenantId', tenant)
+                    order 'orderIndex', 'asc'
+                    maxResults 1
+                }
+            }
+        }
+
+        if (!crmInvoice.currency) {
+            crmInvoice.currency = grailsApplication.config.crm.currency.default ?: "EUR"
+        }
+
+        if (!crmInvoice.invoiceDate) {
+            crmInvoice.invoiceDate = new java.sql.Date(System.currentTimeMillis())
+        }
+
+        // If the order is new or it's status has changed, set the EVENT_CHANGED flag.
+        if (grailsApplication.config.crm.invoice.changeEvent) {
+            def newStatus = crmInvoice.invoiceStatus
+            if (crmInvoice.id == null || oldStatus != newStatus) {
+                crmInvoice.event = CrmInvoice.EVENT_CHANGED
+            }
+        }
+
+        if (crmInvoice.save()) {
+            return crmInvoice
+        } else {
+            // Eager fetch associations to avoid LazyInitializationException
+            crmInvoice.items?.size()
+        }
+
+        throw new CrmValidationException('crmInvoice.validation.error', crmInvoice)
+    }
+
+    private void bindDate(def target, String property, Object value, TimeZone timezone = null) {
+        if (value) {
+            def tenant = crmSecurityService.getCurrentTenant()
+            def locale = tenant?.localeInstance ?: Locale.getDefault()
+            try {
+                if (value instanceof Date) {
+                    target[property] = new java.sql.Date(value.time)
+                } else {
+                    target[property] = DateUtils.parseSqlDate(value.toString(), timezone)
+                }
+            } catch (Exception e) {
+                def entityName = messageSource.getMessage('crmInvoice.label', null, 'Invoice', locale)
+                def propertyName = messageSource.getMessage('crmInvoice.' + property + '.label', null, property, locale)
+                target.errors.rejectValue(property, 'default.invalid.date.message', [propertyName, entityName, value.toString(), e.message].toArray(), "Invalid date: {2}")
+                throw new CrmValidationException('crmInvoice.invalid.date.message', target)
+            }
+        } else {
+            target[property] = null
+        }
+    }
+
+    private void bindItems(CrmInvoice crmInvoice, Map params) {
+        // This is a workaround for Grails 2.4.4 data binding that does not insert a new CrmInvoiceItem when 'id' is null.
+        // I consider this to be a bug in Grails 2.4.4 but I'm not sure how it's supposed to work with Set.
+        // This workaround was not needed in Grails 2.2.4.
+        int i = 0
+        int miss = 0
+        while (miss < 10) {
+            def a = params["items[$i]".toString()]
+            if (a?.id) {
+                def item = CrmInvoiceItem.get(a.id)
+                if (crmInvoice.id != item?.invoiceId) {
+                    throw new RuntimeException("CrmInvoiceItem [${item.invoiceId}] is not associated with CrmInvoice [${crmInvoice.id}]")
+                }
+                grailsWebDataBinder.bind(item, a as SimpleMapDataBindingSource, null, CrmInvoiceItem.BIND_WHITELIST, null, null)
+                item.save()
+            } else if (a) {
+                def item = new CrmInvoiceItem(invoice: crmInvoice)
+                grailsWebDataBinder.bind(item, a as SimpleMapDataBindingSource, null, CrmInvoiceItem.BIND_WHITELIST, null, null)
+                if (!item.isEmpty()) {
+                    if (item.validate()) {
+                        crmInvoice.addToItems(item)
+                    } else {
+                        crmInvoice.errors.addAllErrors(item.errors)
+                    }
+                }
+            } else {
+                miss++
+            }
+            i++
+        }
+    }
+
     /**
      * Update status of an invoice.
      *
@@ -401,6 +597,30 @@ class CrmInvoiceService {
             def payload = crmInvoice.dao
             payload.changes = [number: [before: old, after: crmInvoice.number]]
             event(for: 'crmInvoice', topic: 'updated', data: payload)
+        }
+    }
+
+    List<CrmInvoiceStatus> listInvoiceStatus(String name, Map params = [:]) {
+        CrmInvoiceStatus.createCriteria().list(params) {
+            eq('tenantId', TenantUtils.tenant)
+            if (name) {
+                or {
+                    ilike('name', SearchUtils.wildcard(name))
+                    eq('param', name)
+                }
+            }
+        }
+    }
+
+    List<CrmPaymentTerm> listPaymentTerm(String name, Map params = [:]) {
+        CrmPaymentTerm.createCriteria().list(params) {
+            eq('tenantId', TenantUtils.tenant)
+            if (name) {
+                or {
+                    ilike('name', SearchUtils.wildcard(name))
+                    eq('param', name)
+                }
+            }
         }
     }
 
